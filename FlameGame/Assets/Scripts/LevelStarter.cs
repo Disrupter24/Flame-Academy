e2e71997fa6663b360005ccc2053bbac05b11563
Gabriel Bulcao Rachman <gabrielrachman@hotmail.com>
2021-11-21 17:37:49 -0500

    World Map update
    Star system working (need to be attached to the levels)
    Level UI in world map

--- FlameGame/Assets/Scripts/LevelStarter.cs
+++ FlameGame/Assets/Scripts/LevelStarter.cs
@@ -2,42 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
 public class LevelStarter : MonoBehaviour
 {
+    [Header("Level info")]
+    public Level level;
+    //public bool IsComplete;
+    //[SerializeField] private string _levelName = "[level name]";
+    // bool Star1, Star2, Star3
+
+    [Header("Other levels")]
     public GameObject NextUpLevel;
     public GameObject NextDownLevel;
     public GameObject NextLeftLevel;
     public GameObject NextRightLevel;
 
-    [SerializeField] private Transform _player;
-    [SerializeField] private float speed;
+    [Header("UI Handler")]
+    /*[SerializeField] private GameObject level_UI;
+    [SerializeField] private TextMeshProUGUI text_UI;
+    [SerializeField] private Image[] stars;
+    [SerializeField] private Sprite selectedStar;
+    [SerializeField] private Sprite unselectedStar;*/
+    [SerializeField] private Level_UI level_UI;
 
-    public bool IsComplete;
-    public bool IsMovingUp;
-    public bool IsMovingDown;
-    public bool IsMovingLeft;
-    public bool IsMovingRight;
+    [Header("Player Movement")]
+    [SerializeField] private PlayerMovement_WM _player;
+    
+    private bool IsMovingUp;
+    private bool IsMovingDown;
+    private bool IsMovingLeft;
+    private bool IsMovingRight;
     
 
     private void Start()
     {
-        
+        ChangeCurrentLevel(gameObject);
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) && NextUpLevel != null && (IsComplete || NextUpLevel.GetComponent<LevelStarter>().IsComplete))
+        if (Input.GetKeyDown(KeyCode.W) && NextUpLevel != null && (level.IsComplete || NextUpLevel.GetComponent<LevelStarter>().level.IsComplete))
         {
             IsMovingUp = true;
         }
-        if (Input.GetKeyDown(KeyCode.A) && NextLeftLevel != null && (IsComplete || NextLeftLevel.GetComponent<LevelStarter>().IsComplete))
+        if (Input.GetKeyDown(KeyCode.A) && NextLeftLevel != null && (level.IsComplete || NextLeftLevel.GetComponent<LevelStarter>().level.IsComplete))
         {
             IsMovingLeft = true;
         }
-        if (Input.GetKeyDown(KeyCode.S) && NextDownLevel != null && (IsComplete || NextDownLevel.GetComponent<LevelStarter>().IsComplete))
+        if (Input.GetKeyDown(KeyCode.S) && NextDownLevel != null && (level.IsComplete || NextDownLevel.GetComponent<LevelStarter>().level.IsComplete))
         {
             IsMovingDown = true;
         }
-        if (Input.GetKeyDown(KeyCode.D) && NextRightLevel != null && (IsComplete || NextRightLevel.GetComponent<LevelStarter>().IsComplete))
+        if (Input.GetKeyDown(KeyCode.D) && NextRightLevel != null && (level.IsComplete || NextRightLevel.GetComponent<LevelStarter>().level.IsComplete))
         {
             IsMovingRight = true;
         }
@@ -51,9 +68,10 @@ public class LevelStarter : MonoBehaviour
     {
         if (IsMovingUp)
         {
-            if (_player.transform.position != NextUpLevel.transform.position)
+            if (_player.gameObject.transform.position != NextUpLevel.transform.position)
             {
                 Move(NextUpLevel);
+                level_UI.level_UI.SetActive(false);
             }
             else
             {
@@ -63,9 +81,10 @@ public class LevelStarter : MonoBehaviour
         }
         if (IsMovingLeft)
         {
-            if (_player.transform.position != NextLeftLevel.transform.position)
+            if (_player.gameObject.transform.position != NextLeftLevel.transform.position)
             {
                 Move(NextLeftLevel);
+                level_UI.level_UI.SetActive(false);
             }
             else
             {
@@ -75,9 +94,10 @@ public class LevelStarter : MonoBehaviour
         }
         if (IsMovingDown)
         {
-            if (_player.transform.position != NextDownLevel.transform.position)
+            if (_player.gameObject.transform.position != NextDownLevel.transform.position)
             {
                 Move(NextDownLevel);
+                level_UI.level_UI.SetActive(false);
             }
             else
             {
@@ -87,9 +107,10 @@ public class LevelStarter : MonoBehaviour
         }
         if (IsMovingRight)
         {
-            if (_player.transform.position != NextRightLevel.transform.position)
+            if (_player.gameObject.transform.position != NextRightLevel.transform.position)
             {
                 Move(NextRightLevel);
+                level_UI.level_UI.SetActive(false);
             }
             else
             {
@@ -100,11 +121,24 @@ public class LevelStarter : MonoBehaviour
     }
     private void Move(GameObject target)
     {
-        _player.transform.position = Vector3.MoveTowards(_player.transform.position, target.transform.position, speed * Time.deltaTime);
+        _player.gameObject.transform.position = Vector3.MoveTowards(_player.gameObject.transform.position, target.transform.position, _player.speed * Time.deltaTime);
     }
     private void ChangeCurrentLevel(GameObject target)
-    {
+    {   
+        LevelStarter ls = target.GetComponent<LevelStarter>();
         gameObject.GetComponent<LevelStarter>().enabled = false;
-        target.GetComponent<LevelStarter>().enabled = true;
+        ls.enabled = true;
+
+        //update UI
+        for (int i = 0; i < 3; i++)
+        {
+            level_UI.stars[i].sprite = level_UI.unselectedStar;
+        }
+        for (int i = 0; i < ls.level.starCount; i++)
+        {
+            level_UI.stars[i].sprite = level_UI.selectedStar;
+        }
+        level_UI.text_UI.text = ls.level.LevelName;
+        level_UI.level_UI.SetActive(true);
     }
 }
